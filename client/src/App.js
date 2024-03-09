29c437299c8b3ee9b9fb558fa7dfa32184b2c355
Claire Chu(朱晏禛) <makoto357@gmail.com>
2024-03-09 20:32:32 +0800

    initial commit of the client side app

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <div>hello</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
