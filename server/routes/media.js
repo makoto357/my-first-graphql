03e0104aa656f291f11120cfd170cb629fc0057f
Claire Chu(朱晏禛) <makoto357@gmail.com>
2024-07-28 21:07:23 +0800

    add media route

--- server/routes/media.js
+++ server/routes/media.js
@@ -1,32 +1,63 @@
-const express = require('express');
+const express = require("express");
+const multer = require("multer");
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 const router = express.Router();
-const upload = require('../middlewares/uploadMedia');
-const Video = require('../models/Video');
+const Media = require("../models/Media");
+const S3Service = require("../services/S3Service");
+const s3Service = new S3Service();
 
 // POST using the upload middleware
-router.post('/media', upload.array('files', 5), async (req, res) => {
-  try {
-    // Create a new Video object with the uploaded video details
-    const video = new Video();
-    video.title = req.body.title;
-    video.url = [];
-    // multiple file
-    req.files.forEach(file => {
-      video.url.push(file.location);
-    });
+router.post(
+  "/media",
+  upload.fields([
+    { name: "images", maxCount: 5 },
+    { name: "videos", maxCount: 5 },
+  ]),
+  async (req, res) => {
+    try {
+      const media = new Media();
+      media.title = req.body.title;
+      media.videoUrls = [];
+      media.images = [];
 
-    const savedVideo = await video.save();
+      // multiple file
+      const timestamp = Date.now().toString();
+      const videoKey = `videos/${timestamp}`;
+      const imageKey = `images/${timestamp}`;
 
-    res.json({
-      message: 'Video uploaded successfully',
-      media: savedVideo,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      error: 'An error occurred while uploading the video',
-    });
+      const videoUrls = await s3Service.uploadVideos(
+        req.files,
+        "claireawsbucket",
+        "posts",
+        videoKey
+      );
+      videoUrls.map((url) => {
+        media.videoUrls.push(url);
+      });
+
+      const images = await s3Service.uploadImages(
+        req.files,
+        "claireawsbucket",
+        "posts",
+        imageKey
+      );
+      images.map((url) => {
+        media.images.push(url);
+      });
+
+      const savedMedia = await media.save();
+      res.json({
+        savedMedia,
+        message: "Media uploaded successfully",
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        error: "An error occurred while uploading the video",
+      });
+    }
   }
-});
+);
 
 module.exports = router;
