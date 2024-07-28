04034b0acff12ff185a5dcf7804ecc1157d80a3d
Claire Chu(朱晏禛) <makoto357@gmail.com>
2024-07-28 23:44:41 +0800

    revised vite config

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -2,14 +2,32 @@ import path from 'path';
 import {defineConfig} from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
 
+// corsMiddleware.js
+export function corsMiddleware() {
+  return {
+    name: 'cors',
+    async configureServer(server) {
+      server.middlewares.use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+        next();
+      });
+    },
+  };
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [react(), corsMiddleware()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  define: {
+    global: {},
   },
   test: {
     globals: true,
